33bd8667fdb696403412d1c8ed3c78f252d6dfe6
Julio Diaz & Jordan Johansen <student@epicodus.com>
2015-09-14 10:44:28 -0700

    Add jQuery for displaying extra address forms.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+	$("add-address").click(function() {
+		$("#new-addresses").append('<div class="new_address">' +
+										'<div class="form-group">' +
+											'<label for="new-street">Street></label>' + 
+											'<input type="text" class="form-control new-street">' +
+										'</div>' +
+										'<div class="form-group">' +
+											'<label for="new-city">City</label>' +
+											'<input type="text" class="form-control new-city">' +
+											'</div>' +
+											'<div clas="form-group">' +
+												'<label for="new-state">State</label>' +
+												'<input type="text" class="form-control new-state">' +
+											'</div>' +
+										'</div>');
+	
+	});
+	
 	$("form#new-contact").submit(function(event) {
 		event.preventDefault();
 		
