b4ebd28f8319f45182f4d0fb708d5a5d5a6ff996
Julio Diaz & Jordan Johansen <student@epicodus.com>
2015-09-14 11:15:54 -0700

    Remove defunct address field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	$("add-address").click(function() {
-		$("#new-addresses").append('<div class="new_address">' +
+	$("#add-address").click(function() {
+		$("#new-addresses").append('<div class="new-address">' +
 										'<div class="form-group">' +
-											'<label for="new-street">Street></label>' + 
+											'<label for="new-street">Street</label>' + 
 											'<input type="text" class="form-control new-street">' +
 										'</div>' +
 										'<div class="form-group">' +
 											'<label for="new-city">City</label>' +
 											'<input type="text" class="form-control new-city">' +
-											'</div>' +
-											'<div clas="form-group">' +
-												'<label for="new-state">State</label>' +
-												'<input type="text" class="form-control new-state">' +
-											'</div>' +
-										'</div>');
+										'</div>' +
+										'<div clas="form-group">' +
+											'<label for="new-state">State</label>' +
+											'<input type="text" class="form-control new-state">' +
+										'</div>' +
+									'</div>');
 	
 	});
 	
