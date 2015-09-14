a7ce887e5f336f96d94a105bb0538ab1355ded90
Julio Diaz & Jordan Johansen <student@epicodus.com>
2015-09-14 10:56:58 -0700

    Add routing for multiple address submission.

--- js/scripts.js
+++ js/scripts.js
@@ -22,21 +22,38 @@ $(document).ready(function() {
 		
 		var inputtedFirstName = $("input#new-first-name").val();
 		var inputtedLastName = $("input#new-last-name").val();
-		var inputtedAddress = $("input#new-address").val();
-		var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress};
+		
+		var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+		
+		$(".new-address").each(function() {
+			var inputtedStreet = $(this).find("input.new-street").val();
+			var inputtedCity = $(this).find("input.new-city").val();
+			var inputtedState = $(this).find("input.new-state").val();
+			
+			var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+			newContact.addresses.push(newAddress);
+		});
 		
 		$("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 		
-		$("input#new-first-name").val("");
-		$("input#new-last-name").val("");
-		$("input#new-address").val("");
-		
 		$(".contact").last().click(function() {
 			$("#show-contact").show();
-			$("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+			
+			$("#show-contact h2").text(newContact.firstName);
 			$(".first-name").text(newContact.firstName);
 			$(".last-name").text(newContact.lastName);
-			$(".address").text(newContact.address);
+			
+			$("ul#addresses").text("");
+			newContact.addresses.forEach(function(address) {
+				$("ul#addresses").append("<li" + address.street + ", " + address.city + ", " + address.state + "</li>");
+			});
 		});
+		
+		
+		$("input#new-first-name").val("");
+		$("input#new-last-name").val("");
+		$("input.new-street").val("");
+		$("input.new-city").val("");
+		$("input.new-state").val("");
 	});
 });
