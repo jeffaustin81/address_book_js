842d748f4cd3b6913cbfdf3b6ae2522b68fd7354
Jordan Johansen & Jeff Austin <student@epicodus.com>
2015-09-15 09:38:28 -0700

    Create first test for Contact constructor.

---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe('Contact', function() {
+    it("creates a new contact with the given specifications", function() {
+        var testContact = new Contact("Rita","Moreno");
+        expect(testContact.firstName).to.equal("Rita");
+        expect(testContact.lastName).to.equal("Moreno");
+        expect(testContact.addresses).to.eql([]);
+    });
+});
