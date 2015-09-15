1e8c886d75edc083adce54cdd89f86bae9c1b1d0
Jordan Johansen & Jeff Austin <student@epicodus.com>
2015-09-15 09:38:44 -0700

    Create Contact constructor to pass first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
+function Contact(firstName, lastName) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.addresses = [];
+};
+
 $(document).ready(function() {
 	$("#add-address").click(function() {
 		$("#new-addresses").append('<div class="new-address">' +
 										'<div class="form-group">' +
-											'<label for="new-street">Street</label>' + 
+											'<label for="new-street">Street</label>' +
 											'<input type="text" class="form-control new-street">' +
 										'</div>' +
 										'<div class="form-group">' +
@@ -14,42 +20,42 @@ $(document).ready(function() {
 											'<input type="text" class="form-control new-state">' +
 										'</div>' +
 									'</div>');
-	
+
 	});
-	
+
 	$("form#new-contact").submit(function(event) {
 		event.preventDefault();
-		
+
 		var inputtedFirstName = $("input#new-first-name").val();
 		var inputtedLastName = $("input#new-last-name").val();
-		
-		var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
-		
+
+		var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
 		$(".new-address").each(function() {
 			var inputtedStreet = $(this).find("input.new-street").val();
 			var inputtedCity = $(this).find("input.new-city").val();
 			var inputtedState = $(this).find("input.new-state").val();
-			
+
 			var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
 			newContact.addresses.push(newAddress);
 		});
-		
+
 		$("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
-		
+
 		$(".contact").last().click(function() {
 			$("#show-contact").show();
-			
+
 			$("#show-contact h2").text(newContact.firstName);
 			$(".first-name").text(newContact.firstName);
 			$(".last-name").text(newContact.lastName);
-			
+
 			$("ul#addresses").text("");
 			newContact.addresses.forEach(function(address) {
 				$("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
 			});
 		});
-		
-		
+
+
 		$("input#new-first-name").val("");
 		$("input#new-last-name").val("");
 		$("input.new-street").val("");
